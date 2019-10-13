494f5bb4998958121de85b04940c2befd9f04e40
ccason1 <conlan.cason9@gmail.com>
2019-10-13 11:55:16 -0600

    Create the Card class

---
+++ Creator/Card.java
@@ -0,0 +1,44 @@
+
+
+public class Card
+{
+    private String front;
+    private String back;
+
+    public Card()
+    {
+
+    }
+
+    public Card(String front, String back)
+    {
+        this.front = front;
+        this.back = back;
+    }
+
+    public void setFront(String front)
+    {
+        this.front = front;
+    }
+
+    public void setBack(String back)
+    {
+        this.back = back;
+    }
+
+    public String getFront()
+    {
+        return front;
+    }
+
+    public String getBack()
+    {
+        return back;
+    }
+
+    public String toString()
+    {
+        return front + " " + back;
+    }
+
+}
